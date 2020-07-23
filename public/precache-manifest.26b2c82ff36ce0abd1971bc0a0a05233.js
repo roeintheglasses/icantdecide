2941752cba9174b574535896d78f877758dfad8d
roeintheglasses <hrkjangir@gmail.com>
2020-07-23 16:03:23 +0530

    Adds PWA support

---
+++ public/precache-manifest.26b2c82ff36ce0abd1971bc0a0a05233.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4595f248b7faef1d2f07d62a03a6457",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d1e711091dcb90594c1",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "6038c8d34b34e8bac2a7",
+    "url": "/static/css/main.04d468be.chunk.css"
+  },
+  {
+    "revision": "5d1e711091dcb90594c1",
+    "url": "/static/js/2.3dd8380c.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/static/js/2.3dd8380c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6038c8d34b34e8bac2a7",
+    "url": "/static/js/main.4d687f03.chunk.js"
+  },
+  {
+    "revision": "5230e5f074b74d50ed0e",
+    "url": "/static/js/runtime-main.e4aeea73.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'normalize.css/normalize.css';
 import './scss/style.scss'
+import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,3 +14,7 @@ ReactDOM.render(
 );
 
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
